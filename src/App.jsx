bee731fe6d31f8cf8c6a9e4e3807ecbeec4538a9
ninad-debug <ninadsamant07@gmail.com>
2024-01-07 19:37:29 +0530

    redux toolkit store && folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { useState, useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+  console.log(url);
+
   useEffect(() => {
     apiTesting();
   }, []);
@@ -9,10 +15,16 @@ function App() {
   const apiTesting = () => {
     fetchDataFromApi("/movie/popular").then((res) => {
       console.log(res);
+      dispatch(getApiConfiguration(res));
     });
   };
 
-  return <div>App</div>;
+  return (
+    <div>
+      App
+      {url?.total_pages}
+    </div>
+  );
 }
 
 export default App;
