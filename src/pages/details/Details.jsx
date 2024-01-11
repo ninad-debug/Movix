d871ae6ff636f8974f17a0ee90d84367d844623c
ninad-debug <ninadsamant07@gmail.com>
2024-01-11 16:35:22 +0530

    Details page - Top cast Section

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,15 +1,20 @@
 import useFetch from "../../hooks/useFetch";
 import { useParams } from "react-router-dom";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
+import Cast from "./cast/Cast";
 import "./style.scss";
 
 const Details = () => {
-  // const { mediaType, id } = useParams();
-  // const { data, loading } = useFetch(`/${mediaType}/${id}`);
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data: credits, loading: creditsLoading } = useFetch(
+    `/${mediaType}/${id}/credits`
+  );
 
   return (
     <div>
-      <DetailsBanner />
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={creditsLoading} />
     </div>
   );
 };
