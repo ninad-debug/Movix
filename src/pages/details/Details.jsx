9861e1c7784ec7a7131b6762293f18d279744397
ninad-debug <ninadsamant07@gmail.com>
2024-01-11 09:17:20 +0530

    Details banner section image && title && rating

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,7 +1,17 @@
+import useFetch from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
 import "./style.scss";
 
 const Details = () => {
-  return <div>Details</div>;
+  // const { mediaType, id } = useParams();
+  // const { data, loading } = useFetch(`/${mediaType}/${id}`);
+
+  return (
+    <div>
+      <DetailsBanner />
+    </div>
+  );
 };
 
 export default Details;
